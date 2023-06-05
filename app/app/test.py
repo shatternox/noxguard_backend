10cedbbbfaf8ef241acfbba0ac829940c7442548
danny-insignia <danny@insignia.vc>
2023-06-05 22:16:33 +0700

    practice tests
    practice tests

---
+++ app/app/test.py
@@ -0,0 +1,16 @@
+"""
+Sampel tests
+"""
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+    def test_add_numbers(self):
+        """
+        Test that two numbers are added together
+        docker-compose run --rm app sh -c "python manage.py test"
+        """
+        res = calc.add(3,8)
+        self.assertEqual(res, 11)
