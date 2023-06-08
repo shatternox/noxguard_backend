11b3d39c32afa171a38b878af4c4e2d957b8bb7f
danny-insignia <danny@insignia.vc>
2023-06-08 13:37:59 +0700

    test
    test

--- app/app/test.py
+++ app/app/test.py
@@ -7,6 +7,7 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
 
+    # prefix test_ is required for test methods
     def test_add_numbers(self):
         """
         Test that two numbers are added together
@@ -14,3 +15,10 @@ class CalcTests(SimpleTestCase):
         """
         res = calc.add(3,8)
         self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        """
+        Test that values are subtracted
+        """
+        res = calc.subtract(5,11)
+        self.assertEqual(res, -6)
